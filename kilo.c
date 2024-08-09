2e915d81fc27f12a0302a53d4cb450d3166f02bf
Christian Antony <Zorangepopcorn@gmail.com>
2024-08-09 08:01:55 -0500

    added a dynamic string thingy

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
 /** includes **/
+#include <string.h>
 #include <errno.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -6,10 +7,12 @@
 #include <unistd.h>
 #include <termios.h>
 #include <sys/ioctl.h>
+
 /* defines */
 #define CTRL_KEY(k) ((k) & 0x1f)
-/* function prototypes */ //man i hate using C purely because of function prototypes... I planned not to use these but the dependencies are getting too confusing....
+#define BUFFER_INIT {NULL,0}
 
+/* function prototypes */ //man i hate using C purely because of function prototypes... I planned not to use these but the dependencies are getting too confusing....
 void initEditor();//initializes editor object that saves stuff about terminal
 void die(const char *s);//everything went wrong, print message for info and kys (the program)
 void closeKilo();//closes everything
@@ -21,7 +24,10 @@ void processKeys();//processes key presses (and calls readkeys to read them)
 void drawRows();//draws the tildes, for now
 void refreshScreen();//working on this one
 void setFlags();//sets terminal flags to the values we need to build and run text editor
-/* structs and stuffs*/
+
+/* struct prototypes*/
+struct editorConf;
+struct abuf;
 struct editorConf{//it'll be easier to store global state this way instead of random vars
     struct termios og_termios;//it's really making me mad that termios doesn't have an n...
     int screenwidth; 
@@ -65,15 +71,11 @@ int getCursorPos(int *rows, int *columns){
         i++;//next character
     }
     buffer[i]='\0';//install the null character at the end of the buffer-- C string!
-    printf("\r\nPointer at: %s\r\n",&buffer[1]);//remember first character is escape, so we skip it.
-    if (buffer[0]!='\x1b' || buffer[1]!='[') {
-        die("getCursorPos");
+    if (buffer[0]!='\x1b' || buffer[1]!='[') {die("getCursorPos");}
+    if (sscanf(&buffer[1],"[%d;%d",rows,columns)!=2){//first char is escape, so skip it
+        return -1;//some kind of error with scanf
     }
-    if (sscanf(&buffer[2], "%d;%d", rows, columns) != 2) {
-        return -1;
-    }
-    readKeys();
-    return -1;
+    return 0;
 }
 
 char readKeys(){
@@ -87,7 +89,7 @@ char readKeys(){
 
 int getWindowSize(int *rows, int *columns){//this way we can get rows and cols to work with directly, instead of having to deal with the window struct
     struct winsize windowsize;
-    if(1 || ioctl(STDOUT_FILENO,TIOCGWINSZ,&windowsize)==-1 || windowsize.ws_col==0){
+    if(ioctl(STDOUT_FILENO,TIOCGWINSZ,&windowsize)==-1 || windowsize.ws_col==0){
         //maybe ioctl doesn't work on this system, try manually...
         if (write(STDOUT_FILENO,"\x1b[999B\x1b[999C",12)!=12) return -1;//an error occurred
         return getCursorPos(rows, columns);
@@ -109,16 +111,23 @@ void processKeys(){
             break;
     }
 }
-void drawrows(){
+void drawrows(struct buffer *buff){
     for (int y=0; y<Editor.screenheight; y++){
-        write(STDOUT_FILENO,"~\r\n",3);
+        bufferAppend(buff,"~",1);
+        if (y!=Editor.screenheight-1){
+            bufferAppend(buff,"\r\n",2);
+        }
     }
 }
 void refreshScreen(){
-    write(STDOUT_FILENO,"\x1b[2J",4);//clears the whole screen--/x1b is escape for operator [2J which clears screen (4 bytes)
-    write(STDOUT_FILENO,"\x1b[H",3);//unlike J, H (cursor to home) is just using its default 
-    drawrows();
-    write(STDOUT_FILENO,"\x1b[H",3);
+    struct buffer buff = BUFFER_INIT;//dynamic string for buffer
+    //clears the whole screen--/x1b is escape for operator [2J which clears screen (4 bytes)
+    bufferAppend(&buff,"\x1b[2J",4 );
+    //unlike J, H (cursor to home) is just using its default 
+    bufferAppend(&buff,"\x1b[H",3 );
+    drawrows(&buff);
+    bufferAppend(&buff,"\x1b[H",3 );
+    write(STDOUT_FILENO,buff.string,buff.len);//buff is not a pointer, thus dot operator
 }
 void setFlags(){
     if ((tcgetattr(STDERR_FILENO,&Editor.og_termios))==-1){die("tcsetattr");} //get terminal state, die if fails
@@ -135,4 +144,32 @@ int main() {
         processKeys();
     }
     return 0;
+}
+/*append buffer (aka dynamic string)*/
+struct buffer{//this will be a dynamic string that we use as a buffer.
+    char *string;
+    int len;
+};
+void bufferAppend(struct buffer *buffer, const char *string,int len ){//len here is the len of s
+    char *new=realloc(buffer->string, buffer->len+len);
+    if (new==NULL) return;//just error checking
+    memcpy(&new[buffer->len],string, len);
+    buffer->string=new;
+    buffer->len+=len;
+    /*
+    Notes:
+    realloc is a little weird. in this function, it could create a new block of memory
+    or it could just extend the current one that buffer->string points to.
+    because of this, it essentially gets rid of the original memory location that
+    buffer->string pointed to. Accessing that before the reassignment to new would
+    mean accessing memory from outside the program's perview. The memory at buffer->string
+    was deleted by realloc by then, so it's just random bits. 
+    When we use memcpy, the contents from string get copied at the location starting
+    from where the null pointer of the string we copied into new (via realloc). 
+    We then reassign this new "*new" value to our buffer->string, and blammo, 
+    our buffer->string pointer is now the *new pointer and points to the new string.
+     */
+}
+void freeBuffer(struct buffer *buffer){
+    free(buffer->string);
 }
