83fb9306761bc205e4685f5489c69cf5b7c2ca34
Christian Antony <Zorangepopcorn@gmail.com>
2024-08-17 23:01:34 -0500

    the cursor can move nowgit add .!

--- kilo.c
+++ kilo.c
@@ -11,8 +11,22 @@
 /* defines */
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define BUFFER_INIT {NULL,0}
+#define VERSION "0.0.1"
 
+/* structs*/
+struct editorConf{//it'll be easier to store global state this way instead of random vars
+    struct termios og_termios;//it's really making me mad that termios doesn't have an n...
+    int cursorx;
+    int cursory;
+    int screenwidth; 
+    int screenheight;
+};
+struct buffer{//this will be a dynamic string that we use as a buffer.
+    char *string;
+    int len;
+};
 /* function prototypes */ //man i hate using C purely because of function prototypes... I planned not to use these but the dependencies are getting too confusing....
+void movecursor(char key);
 void initEditor();//initializes editor object that saves stuff about terminal
 void die(const char *s);//everything went wrong, print message for info and kys (the program)
 void closeKilo();//closes everything
@@ -24,19 +38,16 @@ void processKeys();//processes key presses (and calls readkeys to read them)
 void drawRows();//draws the tildes, for now
 void refreshScreen();//working on this one
 void setFlags();//sets terminal flags to the values we need to build and run text editor
-
-/* struct prototypes*/
-struct editorConf;
-struct abuf;
-struct editorConf{//it'll be easier to store global state this way instead of random vars
-    struct termios og_termios;//it's really making me mad that termios doesn't have an n...
-    int screenwidth; 
-    int screenheight;
-};
+void bufferAppend(struct buffer *buffer, const char *string,int len ); // dynamic string append
+void freeBuffer(struct buffer *buffer); //frees buffer memory
 struct editorConf Editor;
 
 void initEditor(){
     if(getWindowSize(&Editor.screenheight,&Editor.screenwidth)==-1){die("getWindowSize");}
+    //cursor is at position cursorx columns to the right 
+    //and cursor y rows down when initialized
+    Editor.cursorx=0;
+    Editor.cursory=0;
 }
 void die(const char *s){
     write(STDOUT_FILENO,"\x1b[2J",4);
@@ -107,28 +118,81 @@ void processKeys(){
             write(STDOUT_FILENO,"\x1b[H",3);
             exit(0);
             break;
-        case 'b':
+        case 'w':
+        case 'a':
+        case 's':
+        case 'd':
+            movecursor(c);
+            break;
+    }
+}
+void movecursor(char key){
+    switch(key) {
+        case 'a'://cursor left
+            if (Editor.cursorx!=0){
+                Editor.cursorx--;
+            }
+            break;
+        case 'w'://cursor up
+            if (Editor.cursory!=0){
+                Editor.cursory--;
+            }
+            break;
+        case 's': 
+            if (Editor.cursory<Editor.screenheight){
+                Editor.cursory++;
+            }
+            break;
+        case 'd':
+            if (Editor.cursorx<Editor.screenwidth){
+                Editor.cursorx++;
+            }
             break;
     }
 }
 void drawrows(struct buffer *buff){
-    for (int y=0; y<Editor.screenheight; y++){
-        bufferAppend(buff,"~",1);
-        if (y!=Editor.screenheight-1){
+    for (int y=0; y<Editor.screenheight; y++){//for every line in the height
+        if (y != Editor.screenheight/3){//when you're not a third of the way down
+            bufferAppend(buff,"\x1b[2K~",5);//clear the line (and add a tilde)
+        } else {//print the welcome message
+            char welcomeMessage[80];
+            int welcomelen=snprintf(welcomeMessage,sizeof(welcomeMessage),"Welcome to Kilo v.%s",VERSION);
+            if (welcomelen>Editor.screenwidth){welcomelen=Editor.screenwidth;}//snprintf will prolly return 80 (but we can't be sure)
+            else {
+                int padding=(Editor.screenwidth-welcomelen)/2;
+                if (padding){ //if there is padding (aka padding!=0)
+                    bufferAppend(buff,"~",1);
+                    padding--;
+                }
+                while (padding--){ //and fill out the rest of the padding
+                    //note: you can use while(increment--) to decrement to 0 from increment
+                    //this is only possible when increment>0, which is guaranteed here
+                    //since we checked that Editor.screenwidth<=welcomelen before we entered this block
+                    bufferAppend(buff," ",1);
+                }
+            }
+            bufferAppend(buff, welcomeMessage,welcomelen);
+        }
+        if (y!=Editor.screenheight-1){//no extra newline at the end of the terminal
             bufferAppend(buff,"\r\n",2);
         }
     }
 }
 void refreshScreen(){
     struct buffer buff = BUFFER_INIT;//dynamic string for buffer
-    //clears the whole screen--/x1b is escape for operator [2J which clears screen (4 bytes)
-    bufferAppend(&buff,"\x1b[2J",4 );
-    //unlike J, H (cursor to home) is just using its default 
-    bufferAppend(&buff,"\x1b[H",3 );
+    bufferAppend(&buff,"\x1b[?25l",6);//hide cursor, remember bytes are by character
+    bufferAppend(&buff,"\x1b[H",3 );//curor to home
     drawrows(&buff);
-    bufferAppend(&buff,"\x1b[H",3 );
-    write(STDOUT_FILENO,buff.string,buff.len);//buff is not a pointer, thus dot operator
+    bufferAppend(&buff,"\x1b[H",3 );//back to home
+    //instead of moving back home, we're going to move to the x and y
+    char buf_cursorpos[32];
+    snprintf(buf_cursorpos,sizeof(buf_cursorpos),"\x1b[%d;%dH",Editor.cursory+1,Editor.cursorx+1);//the terminal is indexed from 1 for some reason...
+    bufferAppend(&buff,buf_cursorpos,strlen(buf_cursorpos));
+    bufferAppend(&buff,"\x1b[?25h",6);//show the cursor
+    write(STDOUT_FILENO,buff.string,buff.len);//flush the buffer to STDOUT_FILENO
+    freeBuffer(&buff);//frees buff memory!
 }
+
 void setFlags(){
     if ((tcgetattr(STDERR_FILENO,&Editor.og_termios))==-1){die("tcsetattr");} //get terminal state, die if fails
     atexit(closekilo);//stdlib.h function, forces exit function at exit no matter what.
@@ -146,10 +210,6 @@ int main() {
     return 0;
 }
 /*append buffer (aka dynamic string)*/
-struct buffer{//this will be a dynamic string that we use as a buffer.
-    char *string;
-    int len;
-};
 void bufferAppend(struct buffer *buffer, const char *string,int len ){//len here is the len of s
     char *new=realloc(buffer->string, buffer->len+len);
     if (new==NULL) return;//just error checking
